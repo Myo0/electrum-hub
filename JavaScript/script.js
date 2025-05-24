0cf79ef8f55025ba09658a3398ae5f13969a8f74
Myo0 <corbinbrooks03@gmail.com>
2025-05-25 00:13:07 +0800

    External .js file added

---
+++ JavaScript/script.js
@@ -0,0 +1,18 @@
+"use strict";
+    // Pokémon Objects
+let bulbasaur
+
+    // ------------------------------------------------------------------
+document.querySelectorAll('.sub-nav button').forEach(btn => {
+    btn.addEventListener('click', () => {
+
+      document.querySelectorAll('.sub-nav button')
+        .forEach(b => b.classList.remove('active'));
+      btn.classList.add('active');
+  
+      document.querySelectorAll('.content-section')
+        .forEach(sec => sec.classList.remove('active'));
+      document.getElementById(`${btn.dataset.section}-section`)
+        .classList.add('active');
+    });
+  });
