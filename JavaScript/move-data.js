828244bcf5a3b49a74a5c465b32c0c1230224506
Corbin Brooks <corbinbrooks03@gmail.com>
2026-02-20 10:13:47 -0330

    Added 10 more moves to move-data.js

--- JavaScript/move-data.js
+++ JavaScript/move-data.js
@@ -6,10 +6,7 @@ window.moveData = [
         category: "Physical",
         power: 40,
         accuracy: 100,
-        pp: {
-        base: 35,
-        max: 56
-        },
+        pp: { base: 35, max: 56 },
         effect: "", // no special effect
         description: "The foe is physically pounded with a long tail or a foreleg, etc."
     },
@@ -20,11 +17,120 @@ window.moveData = [
         category: "Physical",
         power: 50,
         accuracy: 100,
-        pp: {
-        base: 25,
-        max: 40
-        },
-        effect: "+1 Crit Ratio (1/8)",
+        pp: { base: 25, max: 40 },
+        effect: "+1 Crit Ratio (1/8 or 12.5%)",
         description: "The foe is attacked with a sharp chop. It has a high critical-hit ratio."
+    },
+    {
+        id: "MOVE_DOUBLE_SLAP",
+        name: "Double Slap",
+        type: "Normal",
+        category: "Physical",
+        power: 15,
+        accuracy: 100,
+        pp: { base: 10, max: 16 },
+        effect: "Hits 2–5 times.",
+        description: "The foe is slapped repeatedly, back and forth, two to five times in a row."
+    },
+    {
+        id: "MOVE_COMET_PUNCH",
+        name: "Comet Punch",
+        type: "Normal",
+        category: "Physical",
+        power: 18,
+        accuracy: 100,
+        pp: { base: 15, max: 24 },
+        effect: "Hits 2–5 times.",
+        description: "The foe is hit with a flurry of punches that strike two to five times in a row."
+    },
+    {
+        id: "MOVE_MEGA_PUNCH",
+        name: "Mega Punch",
+        type: "Normal",
+        category: "Physical",
+        power: 80,
+        accuracy: 100,
+        pp: { base: 20, max: 32 },
+        effect: "",
+        description: "The foe is slugged by a punch thrown with muscle-packed power."
+    },
+    {
+        id: "MOVE_PAY_DAY",
+        name: "Pay Day",
+        type: "Normal",
+        category: "Physical",
+        power: 40,
+        accuracy: 100,
+        pp: { base: 20, max: 32 },
+        effect: "Earns money after battle.",
+        description: "Numerous coins are hurled at the foe to inflict damage. Money is earned after battle."
+    },
+    {
+        id: "MOVE_FIRE_PUNCH",
+        name: "Fire Punch",
+        type: "Fire",
+        category: "Physical",
+        power: 75,
+        accuracy: 100,
+        pp: { base: 15, max: 24 },
+        effect: "10% chance to burn.",
+        description: "The foe is punched with a fiery fist. It may leave the target with a burn."
+    },
+    {
+        id: "MOVE_ICE_PUNCH",
+        name: "Ice Punch",
+        type: "Ice",
+        category: "Physical",
+        power: 75,
+        accuracy: 100,
+        pp: { base: 15, max: 24 },
+        effect: "10% chance to freeze.",
+        description: "The foe is punched with an icy fist. It may leave the target frozen."
+    },
+    {
+        id: "MOVE_THUNDER_PUNCH",
+        name: "Thunder Punch",
+        type: "Electric",
+        category: "Physical",
+        power: 75,
+        accuracy: 100,
+        pp: { base: 15, max: 24 },
+        effect: "10% chance to paralyze.",
+        description: "The foe is punched with an electrified fist. It may leave the target with paralysis."
+    },
+    {
+        id: "MOVE_SCRATCH",
+        name: "Scratch",
+        type: "Normal",
+        category: "Physical",
+        power: 40,
+        accuracy: 100,
+        pp: { base: 35, max: 56 },
+        effect: "",
+        description: "Hard, pointed, and sharp claws rake the foe to inflict damage."
+    },
+    {
+        id: "MOVE_VISE_GRIP",
+        name: "Vise Grip",
+        type: "Normal",
+        category: "Physical",
+        power: 55,
+        accuracy: 100,
+        pp: { base: 30, max: 48 },
+        effect: "",
+        description: "Huge, impressive pincers grip and squeeze the foe."
+    },
+    {
+        id: "MOVE_GUILLOTINE",
+        name: "Guillotine",
+        type: "Normal",
+        category: "Physical",
+        power: "—",
+        accuracy: 30,
+        pp: { base: 5, max: 8 },
+        effect: "One-hit KO move.",
+        description: "A vicious, tearing attack with pincers. The foe will faint instantly if this attack hits."
     }
+
+
 ];
